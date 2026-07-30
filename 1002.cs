aaed6c1557295d058829f65b4665ac7fd2f6e4e6
katiatamaishi-create <katia.tamaishi@alunos.ifsuldeminas.edu.br>
2026-07-30 14:04:16 -0300

    Add program to print even numbers from 1 to 100

---
+++ 1002.cs
@@ -0,0 +1,15 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
